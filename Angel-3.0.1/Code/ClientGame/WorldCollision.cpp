234805a54a8bb8976b1cfa327caf7e2a6078c276
Philipp Hufnagl <philipp.hufnagl@gmx.net>
2012-11-14 12:34:51 +0000

    collision working correctly now

--- Angel-3.0.1/Code/ClientGame/WorldCollision.cpp
+++ Angel-3.0.1/Code/ClientGame/WorldCollision.cpp
@@ -6,25 +6,26 @@ WorldCollision::WorldCollision(const String& arg)
 {//tga anschauen
 	//stb_image
 	int i;
-	int x, y, type;
+	int x=800, y=600, type=1;
 	m_mask = stbi_load(arg.c_str(),&x,&y,&type,0);
 	if (m_mask == NULL) std::cout<<"cant inizialise mask"<<std::endl;
 	else
-	std::cout<<"x:"<<x<<" y:"<<y<<" type:"<<type<<std::endl;
+	std::cout<<"x:"<<x<<" y:"<<y<<" type:"<<type<<std::endl<<"array: "<<sizeof(m_mask)<<std::endl;
 	//m_mask= new char[800*600];
 		
 
 }
 bool WorldCollision::isCollideing(const Vector2& argBegin,const  Vector2& argEnd) const{
 	int i,j;//enum
-	
+	Vector2 begin(MathUtil::WorldUnitsToPixels(argBegin.X) +(SCREEN_X/2),MathUtil::WorldUnitsToPixels(argBegin.Y) +(SCREEN_Y/2));
+	Vector2 end(ceil(MathUtil::WorldUnitsToPixels(argEnd.X)) +(SCREEN_X/2),ceil(MathUtil::WorldUnitsToPixels(argEnd.Y)) +(SCREEN_Y/2));
+	std::cout<<"checking "<<begin.X<<", "<<begin.Y<<std::endl;
 			//const
-	for(i= MathUtil::WorldUnitsToPixels(argBegin.X);i < ceil(MathUtil::WorldUnitsToPixels(argEnd.X));i++)
-			for(j= MathUtil::WorldUnitsToPixels(argBegin.Y);j <  ceil(MathUtil::WorldUnitsToPixels(argEnd.Y));j++)
-				if(m_mask[800*(i+SCREEN_X /2)+(j+SCREEN_Y /2)] == 0) 
+	for(i= begin.X; i< end.X; i++)
+		for(j= begin.Y; j < end.Y;j++)
+				if(m_mask[800*600-(800*(j+1)-i)] == 0) //mirrored about both axes
 				{
-					std::cout<<"found collision! X: "<<i<<" Y: "<<j<<std::endl
-					<<"Mask coordinates: X: " << i+SCREEN_X /2<< " Y: "<<(j+SCREEN_Y /2)<< std::endl; 
+					std::cout<<"found collision! X: "<<i<<" Y: "<<j<<std::endl; 
 					return true;
 				} //end here by collision
 	//runs only here when there is no collision*/
